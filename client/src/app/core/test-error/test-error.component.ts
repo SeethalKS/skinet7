cd3570ae933e2302e9f87de2e00574163258dfb5
SeethalKS <seethal007@outlook.com>
2023-10-17 09:47:43 +0530

    end of section 11

---
+++ client/src/app/core/test-error/test-error.component.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrls: ['./test-error.component.scss']
+})
+export class TestErrorComponent {
+baseurl=environment.apiUrl;
+//validationErrors: string[] =[]; class 120
+
+constructor(private http:HttpClient){}
+
+get404Error(){
+  this.http.get(this.baseurl + 'products/42').subscribe({
+    next: response =>console.log(response),
+    error:error => console.log(error)
+    })
+}
+get500Error(){
+  this.http.get(this.baseurl + 'buggy/servererror').subscribe({
+    next: response =>console.log(response),
+    error:error => console.log(error)
+    })
+}
+get400Error(){
+  this.http.get(this.baseurl + 'buggy/badrequest').subscribe({
+    next: response =>console.log(response),
+    error:error => console.log(error)
+    })
+}
+get400ValidationError(){
+  this.http.get(this.baseurl + 'products/fortytwo').subscribe({
+    next: response =>console.log(response),
+    error:error => console.log(error)
+      //this.validationErrors=error.errors; class 120}
+    })
+}
+}
