25b6a97a80f152b843cf966b140a1549a6a9babe
SeethalKS <seethal007@outlook.com>
2023-10-23 18:17:57 +0530

    end of section 13

--- API/Extensions/ApplicationServicesExtension.cs
+++ API/Extensions/ApplicationServicesExtension.cs
@@ -22,7 +22,12 @@ services.AddDbContext<StoreContext>(opt =>
     opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
 });
 
-services.AddSingleton<IConnectionMultiplexer>();
+services.AddSingleton<IConnectionMultiplexer>(c =>
+{
+    var options=ConfigurationOptions.Parse(config.GetConnectionString("Redis"));
+    return ConnectionMultiplexer.Connect(options);
+});
+services.AddScoped<IBasketRepository,BasketRepository>();
 services.AddScoped<IProductRepository,ProductRepository>();
 services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
 
